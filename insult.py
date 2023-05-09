27a9caa203302f82fdc26e245e218b6a0513112e
Chad Frost <chad.r.frost@gmail.com>
2023-05-09 08:37:00 -0600

    added speech output

--- insult.py
+++ insult.py
@@ -1,6 +1,7 @@
 #!python3
 
 import random
+from os import system
 
 col1 = ['artless', 'bawdy', 'beslubbering', 'bootless', 'churlish', 'cockered', 'clouted', 'craven', 'currish', 'dankish', 'dissembling', 'droning', 'errant', 'fawning', 'fobbing', 'froward', 'frothy', 'gleeking', 'goatish', 'gorbellied', 'impertinent', 'infectious', 'jarring', 'loggerheaded', 'lumpish', 'mammering', 'mangled', 'mewling', 'paunchy', 'pribbling', 'puking', 'puny', 'qualling', 'rank', 'reeky', 'roguish', 'ruttish', 'saucy', 'spleeny', 'spongy', 'surly', 'tottering', 'unmuzzled', 'vain', 'venomed', 'villainous', 'warped', 'wayward', 'weedy', 'yeasty']
 
@@ -8,4 +9,8 @@ col2 = ['base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brain
 
 col3 = ['apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlot', 'vassal', 'whey-face', 'wagtail']
 
-print("Thou {} {} {}!".format(random.choice(col1),random.choice(col2),random.choice(col3)))
+
+str = "Thou {} {} {}!".format(random.choice(col1),random.choice(col2),random.choice(col3))
+print(str)
+
+system('say %s' % (str)) 
